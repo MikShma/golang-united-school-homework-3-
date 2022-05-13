1c0764a0527e34718e46f97599e2b32cacc32461
Mikita Shmatko <mikita.shmatko@syngenta.com>
2022-05-13 14:46:33 +0400

    first

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,14 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	var summ, count float32 = 0.0, 0.0
+	for _, n := range input {
+		if n == 0 {
+			continue
+		}
+		summ += n
+		count += 1
+	}
+	result = summ / count
 	return
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,16 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
+	k := []int{}
+	for i := range input {
+		k = append(k, i)
+	}
+	sort.Ints(k)
+	for i := range k {
+		result = append(result, input[i])
+	}
 	return
 }
